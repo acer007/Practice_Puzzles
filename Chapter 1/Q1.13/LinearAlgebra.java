e397a234e954ad7e38aae6f4029910cc74ac0e81
Nehla <nnamin1446@gmail.com>
2016-09-06 16:17:11 +0000

    Initial Commit

---
+++ Chapter 1/Q1.13/LinearAlgebra.java
@@ -0,0 +1,13 @@
+// Solve a linear algebra problem
+
+public class LinearAlgebra{
+    
+    public static void main (String args[]){
+        
+        //Display equation
+        System.out.println("3.4x + 50.2y = 44.5 \n" + 
+                            "2.1x + 0.55y = 5.9");
+        
+    }//end of main
+    
+}//end of class
