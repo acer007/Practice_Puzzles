9a4e1d359e573aeb749e502b2c22c174847c5c92
Nehla <nnamin1446@gmail.com>
2016-09-07 15:27:16 +0000

    Initial commit

---
+++ Chapter 2/Q2.7/NumberOfYears.java
@@ -0,0 +1,14 @@
+//Allow user to enter the number of minutes
+//Program will convert minutes to the number of years and days
+
+import java.util.Scanner;
+
+public class NumberOfYears{
+    
+    public static void main (String args[]){
+        
+        
+        
+    }//end of main
+    
+}//end of class
