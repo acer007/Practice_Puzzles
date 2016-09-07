cb5c900c035210c68b1d9d6c78d41889f992f144
Nehla <nnamin1446@gmail.com>
2016-09-07 16:30:21 +0000

    Added try-catch block & Verified Output

--- Chapter 2/Q2.13/CompoundInterest.java
+++ Chapter 2/Q2.13/CompoundInterest.java
@@ -7,19 +7,58 @@ public class CompoundInterest{
     
     public static void main (String args[]){
         
-        //Declare Scanner
-        Scanner scanInput = new Scanner(System.in);
+        try{
+            
+            //Declare Scanner
+            Scanner scanInput = new Scanner(System.in);
+            
+            //Prompt input
+            System.out.println("Compound Interest Calculator" + 
+                                "\n---------------------------------------" + 
+                                "-----------------------------------------" + 
+                                "-----------------\n"
+                                + "\tPurpose: Program calculates the account " + 
+                                "balance after 6 months given the specified \n" + 
+                                "\tamount deposited by the user per month and " + 
+                                "the annual interest rate of 5.00%");
+                                
+            System.out.print("\n\tEnter the amount of money saved into the account per month: $");
+            double money = 0;
+            
+            //Verify Input
+            while (scanInput.hasNext()){
+                
+                if (scanInput.hasNextDouble()){
+                    money = scanInput.nextDouble();
+                    
+                    if (money < 0){
+                        Double badInputDoub = money;
+                        System.out.print("\n\tRe-enter the balance: $");
+                        
+                    }
+                    else
+                        break;
+                }
+                else{
+                    String badInputStr = scanInput.next();
+                    System.out.print("\n\tRe-enter the balance: $");
+                }
+                
+            }
+            
+            //Calculate account balance
+            double monthlyInt = 0.05 / 12;
+            double amount = money;
+            double balance = 0;
+            
+            
+            //Display
         
-        //Prompt input
-        System.out.println("Compound Interest Calculator" + 
-                            "\n---------------------------------------------\n"
-                            + "\tPurpose: Program calculates the account " + 
-                            "balance after 6 months given the specified \n" + 
-                            "\tamount deposited by the user per month and " + 
-                            "the annual interest rate of 5.00%");
-                            
-        System.out.print("Enter the amount of money saved into the account per month: ");
+        }
         
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
         
     }//end of main
     
