2828f511d4425561a7d989f4a2f06d7ff08eb7d2
Nehla <nnamin1446@gmail.com>
2016-09-07 14:23:12 +0000

    Verified input

--- Chapter 2/Q2.6/SumAllDigits.java
+++ Chapter 2/Q2.6/SumAllDigits.java
@@ -6,7 +6,42 @@ public class SumAllDigits{
     
     public static void main (String args[]){
         
+        //Declare Scanner
+        Scanner scanInput = new Scanner(System.in);
         
+        //Prompt input
+        System.out.println("Enter a number between 0 and 1000 and I will take " 
+                            + "the sum of its digits: ");
+        
+        //Check input
+        while (scanInput.hasNext()){
+            
+            if (scanInput.hasNextInt()){
+                
+                int num = scanInput.nextInt();
+                
+                if (num < 0 || num > 1000){
+                    
+                    int badInputInt = num;
+                    System.out.println("Please enter a number between 0 and " + 
+                                        "1000");
+                    
+                }
+                
+                else{
+                    break;
+                }
+                
+            }
+            
+            else{
+                
+                String badInputStr = scanInput.next();
+                System.out.println("Please enter a number between 0 and 1000");
+                
+            }
+            
+        }
         
     }//end of main
     
