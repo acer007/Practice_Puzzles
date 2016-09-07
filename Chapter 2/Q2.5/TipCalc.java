61fa8d1b4b33a7b6a9c3934d4d58fe3fd00e6aab
Nehla <nnamin1446@gmail.com>
2016-09-07 03:27:25 +0000

    Initial Commit

---
+++ Chapter 2/Q2.5/TipCalc.java
@@ -0,0 +1,13 @@
+//Calculate the tip and total cost given the subtotal and gratuity rate
+
+import java.util.Scanner;
+
+public class TipCalc{
+    
+    public static void main (String args[]){
+        
+        
+        
+    }//end of main
+    
+}//end of class
