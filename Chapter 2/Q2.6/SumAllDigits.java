079078433ae1baf0f3d2b7da8f163e1c9be09365
Nehla <nnamin1446@gmail.com>
2016-09-07 14:15:22 +0000

    Initial Commit

---
+++ Chapter 2/Q2.6/SumAllDigits.java
@@ -0,0 +1,13 @@
+//Find the sum of all the digits in a number between 0 and 1000
+
+import java.util.Scanner;
+
+public class SumAllDigits{
+    
+    public static void main (String args[]){
+        
+        
+        
+    }//end of main
+    
+}//end of class
