8ad1478d38b52e846a7280a7ab40e8a84f604a2c
Nehla <nnamin1446@gmail.com>
2016-09-07 16:13:02 +0000

    Initial Commit

---
+++ Chapter 2/Q2.13/CompoundInterest.java
@@ -0,0 +1,26 @@
+//User enters a monthly saving amount
+//Program calculates the account balance after the 6th month 
+
+import java.util.Scanner;
+
+public class CompoundInterest{
+    
+    public static void main (String args[]){
+        
+        //Declare Scanner
+        Scanner scanInput = new Scanner(System.in);
+        
+        //Prompt input
+        System.out.println("Compound Interest Calculator" + 
+                            "\n---------------------------------------------\n"
+                            + "\tPurpose: Program calculates the account " + 
+                            "balance after 6 months given the specified \n" + 
+                            "\tamount deposited by the user per month and " + 
+                            "the annual interest rate of 5.00%");
+                            
+        System.out.print("Enter the amount of money saved into the account per month: ");
+        
+        
+    }//end of main
+    
+}//end of class
