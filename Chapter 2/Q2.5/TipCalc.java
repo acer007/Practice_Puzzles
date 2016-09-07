9528bc5106310acf0e854f2c6e4fb4fded76625c
Nehla <nnamin1446@gmail.com>
2016-09-07 14:11:29 +0000

    Added try-catch block

--- Chapter 2/Q2.5/TipCalc.java
+++ Chapter 2/Q2.5/TipCalc.java
@@ -6,82 +6,90 @@ public class TipCalc{
     
     public static void main (String args[]){
         
-        //Declare Scanner
-        Scanner scanInput = new Scanner(System.in);
-        
-        //Prompt input
-        System.out.print("Please enter the subtotal (Ex: 10.56): ");
-        double subTotal = 0;
-        
-        //Check input
-        while (scanInput.hasNext()){
+        try{
             
-            if (scanInput.hasNextDouble()){
+            //Declare Scanner
+            Scanner scanInput = new Scanner(System.in);
+            
+            //Prompt input
+            System.out.print("Please enter the subtotal (Ex: 10.56): ");
+            double subTotal = 0;
+            
+            //Check input
+            while (scanInput.hasNext()){
                 
-                subTotal = scanInput.nextDouble();
-                
-                if (subTotal < 0){
+                if (scanInput.hasNextDouble()){
                     
-                    double badInputInt = subTotal;
+                    subTotal = scanInput.nextDouble();
+                    
+                    if (subTotal < 0){
+                        
+                        double badInputInt = subTotal;
+                        System.out.println("Please re-enter the subtotal: ");
+                        
+                    }
+                    
+                    else{
+                        break;
+                    }
+                    
+                }
+                
+                else{
+                    
+                    String badInputStr = scanInput.next();
                     System.out.println("Please re-enter the subtotal: ");
                     
                 }
                 
-                else{
-                    break;
+            }
+            
+            System.out.print("\nPlease enter the tip percentage (Ex: 15.50): ");
+            double tip = 0;
+            
+            while (scanInput.hasNext()){
+                
+                if (scanInput.hasNextDouble()){
+                    
+                    tip = scanInput.nextDouble();
+                    
+                    if (tip < 0){
+                        
+                        double badInputInt = tip;
+                        System.out.println("Please re-enter the tip: ");
+                        
+                    }
+                    
+                    else{
+                        break;
+                    }
+                    
                 }
                 
-            }
-            
-            else{
-                
-                String badInputStr = scanInput.next();
-                System.out.println("Please re-enter the subtotal: ");
-                
-            }
-            
-        }
-        
-        System.out.print("\nPlease enter the tip percentage (Ex: 15.50): ");
-        double tip = 0;
-        
-        while (scanInput.hasNext()){
-            
-            if (scanInput.hasNextDouble()){
-                
-                tip = scanInput.nextDouble();
-                
-                if (tip < 0){
+                else{
                     
-                    double badInputInt = tip;
+                    String badInputStr = scanInput.next();
                     System.out.println("Please re-enter the tip: ");
                     
                 }
                 
-                else{
-                    break;
-                }
-                
             }
             
-            else{
-                
-                String badInputStr = scanInput.next();
-                System.out.println("Please re-enter the tip: ");
-                
-            }
+            //Calculate Tip and Total
+            double tipDecimal = tip / 100;
+            double tipMoney = ((int)((tipDecimal * subTotal) * 100)) / 100.00;
+            double total = ((int)((subTotal + tipMoney) * 100)) / 100.00;
+            
+            //Display Output
+            System.out.println("For a " + tip + "%, the tip is $" + tipMoney + 
+                                " and the total is $" + total);
             
         }
-        
-        //Calculate Tip and Total
-        double tipDecimal = tip / 100;
-        double tipMoney = ((int)((tipDecimal * subTotal) * 100)) / 100.00;
-        double total = subTotal + tipMoney;
-        
-        //Display Output
-        System.out.println("For a " + tip + "%, the tip is $" + tipMoney + 
-                            " and the total is $" + total);
-        
+        catch(Exception e){
+            
+            System.out.println(e.getMessage());
+            
+        }
     }//end of main
     
 }//end of class
