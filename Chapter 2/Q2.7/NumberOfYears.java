d3de2212c51b2c20d33dad5f922682fdad27bb4f
Nehla <nnamin1446@gmail.com>
2016-09-07 15:39:35 +0000

    Added try-catch block & Verified Input

--- Chapter 2/Q2.7/NumberOfYears.java
+++ Chapter 2/Q2.7/NumberOfYears.java
@@ -7,7 +7,51 @@ public class NumberOfYears{
     
     public static void main (String args[]){
         
+        try{
+            
+            //Declare scanner
+            Scanner scanInput = new Scanner(System.in);
+            
+            //Prompt input
+            System.out.println("Minutes Converter" + 
+                                "\n--------------------------------\n");
+            System.out.print("Enter the number of minutes: ");
+            
+            //Verify Input
+            while (scanInput.hasNext()){
+                
+                if (scanInput.hasNextInt()){
+                    
+                    int minutes = scanInput.nextInt();
+                    
+                    if (minutes < 0){
+                        
+                        int badInputInt = minutes;
+                        System.out.println("Re-enter the number of minutes " + 
+                                    "as a positive whole number: ");
+                    }
+                    
+                    else
+                        break;
+                    
+                }
+                
+                else{
+                    
+                    String badInputStr = scanInput.next();
+                    System.out.print("Enter the number of minutes as a " + 
+                                        "whole number: ");
+                    
+                }
+            }
+            
+            
         
+        }
+        
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
         
     }//end of main
     
