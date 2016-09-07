3eb37ef37643b2a03ae110cb04c0df141c91268f
Nehla <nnamin1446@gmail.com>
2016-09-07 15:09:29 +0000

    Finding the sum through a for loop

--- Chapter 2/Q2.6/SumAllDigits.java
+++ Chapter 2/Q2.6/SumAllDigits.java
@@ -10,21 +10,23 @@ public class SumAllDigits{
         Scanner scanInput = new Scanner(System.in);
         
         //Prompt input
-        System.out.println("Enter a number between 0 and 1000 and I will take " 
+        System.out.print("Enter a number between 0 and 1000 and I will take " 
                             + "the sum of its digits: ");
+        
+        int num = 0;
         
         //Check input
         while (scanInput.hasNext()){
             
             if (scanInput.hasNextInt()){
                 
-                int num = scanInput.nextInt();
+                num = scanInput.nextInt();
                 
-                if (num < 0 || num > 1000){
+                if (num <= 0 || num >= 1000){
                     
                     int badInputInt = num;
-                    System.out.println("Please enter a number between 0 and " + 
-                                        "1000");
+                    System.out.print("\nPlease enter a number between 0 and " + 
+                                        "1000: ");
                     
                 }
                 
@@ -37,11 +39,25 @@ public class SumAllDigits{
             else{
                 
                 String badInputStr = scanInput.next();
-                System.out.println("Please enter a number between 0 and 1000");
+                System.out.print("\nPlease enter a number between 0 and 1000: ");
                 
             }
             
         }
+        
+        int sum = 0;
+        double digit = 0;
+        
+        for (int k = 3; k > 0 ; k--){
+            
+            digit = (num % Math.pow(10, k)) - (num % Math.pow(10, k-1));
+            System.out.println("Digit: " + digit);
+            
+            sum += digit;
+            System.out.println("Sum: " + sum);
+        }
+        
+        System.out.println("The sum of all its digits is " + sum);
         
     }//end of main
     
