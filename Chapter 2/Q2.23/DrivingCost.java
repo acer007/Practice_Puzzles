45dbfa460a10a417500a2482d4d1d97d49c23ad3
Nehla <nnamin1446@gmail.com>
2016-09-08 23:23:05 +0000

    Added calculateCost method & Completed program

--- Chapter 2/Q2.23/DrivingCost.java
+++ Chapter 2/Q2.23/DrivingCost.java
@@ -10,12 +10,12 @@ public class DrivingCost{
         
             //Declare Scanner and Variables
             Scanner scanInput = new Scanner(System.in);
-            double distance = -1;
-            //, milesPerGallon, pricePerGallon, cost;
-            boolean sentinel1 = true;
+            double distance = -1, milesPerGallon = -1, pricePerGallon = -1, cost = -1;
+            boolean sentinel1 = true, sentinel2 = true, sentinel3 = true;
             
             //Prompt for input
             System.out.print("Enter the driving distance in miles: ");
+            
             while (sentinel1){
                 distance = verifyInput(scanInput);
                 //System.out.println("\nDistance: " + distance);
@@ -23,6 +23,30 @@ public class DrivingCost{
                     sentinel1 = false;
                 }
             }
+            
+            System.out.print("Enter the miles per gallon: ");
+            
+            while (sentinel2){
+                milesPerGallon = verifyInput(scanInput);
+                //System.out.println("\nDistance: " + distance);
+                if (milesPerGallon != -1){
+                    sentinel2 = false;
+                }
+            }
+            
+            System.out.print("Enter the price per gallon: $");
+            
+            while (sentinel3){
+                pricePerGallon = verifyInput(scanInput);
+                //System.out.println("\nDistance: " + distance);
+                if (pricePerGallon != -1){
+                    sentinel3 = false;
+                }
+            }
+            
+            //Display
+            cost = calculateCost(distance, milesPerGallon, pricePerGallon);
+            System.out.println("The cost of driving is $" + cost);
         
         }
         
@@ -60,7 +84,11 @@ public class DrivingCost{
         
     }
     
-    
     //Calculation
+    public static double calculateCost (double x, double y, double z){
+        
+        double amount = ((int)(x / y * z * 100)) / 100.00;
+        return amount;
+    }
     
 }//end of class
