ad075095dcd135579c4801681180a5d969053c0a
Nehla <nnamin1446@gmail.com>
2016-09-08 14:30:56 +0000

    Add try-catch block & Display output

--- Chapter 2/Q2.18/PrintTable.java
+++ Chapter 2/Q2.18/PrintTable.java
@@ -4,7 +4,29 @@ public class PrintTable{
     
     public static void main (String args[]){
         
+        try{
+            
+            //Print a bold heading
+            String header = "\033[1ma \t b \t pow(a, b) \033[0m";
+            System.out.println(header);
+            
+            //Declare Variables
+            int a, b, c;
+            
+            //Display
+            for (int k = 1; k <=5; k++ ){
+                a = k;
+                b = a+1;
+                c = (int)Math.pow(a, b);
+                System.out.printf("%d \t %d \t %d \n",a, b, c);
+                
+            }
         
+        }
+        
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
         
     }//end of main
     
