3edf85f7af3992e17d8e847d81d3b1aa5302a4f6
Nehla <nnamin1446@gmail.com>
2016-09-08 14:35:29 +0000

    Initial Commit

---
+++ Chapter 2/Q2.23/DrivingCost.java
@@ -0,0 +1,21 @@
+//Calculate the cost of driving given the prompt for user input
+
+import java.util.Scanner;
+
+public class DrivingCost{
+    
+    public static void main (String args[]){
+        
+        try{
+            
+            
+            
+        }
+        
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        
+    }//end of main
+    
+}//end of class
