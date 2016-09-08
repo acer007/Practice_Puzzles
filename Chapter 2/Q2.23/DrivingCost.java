fc1e497b0a8ae103dc44b779794b1da4b6bf3c53
Nehla <nnamin1446@gmail.com>
2016-09-08 15:18:03 +0000

    Added verifyInput method

--- Chapter 2/Q2.23/DrivingCost.java
+++ Chapter 2/Q2.23/DrivingCost.java
@@ -7,9 +7,20 @@ public class DrivingCost{
     public static void main (String args[]){
         
         try{
+        
+            //Declare Scanner and Variables
+            Scanner scanInput = new Scanner(System.in);
+            double distance=0;
+            //, milesPerGallon, pricePerGallon, cost;
+            boolean sentinel1 =true;
             
-            
-            
+            //Prompt for input
+            System.out.print("Enter the driving distance in miles: ");
+            while (sentinel1){
+                sentinel1 = verifyInput(scanInput, distance);
+                System.out.println("\nDistance: " + distance);
+            }
+        
         }
         
         catch (Exception e){
@@ -17,5 +28,33 @@ public class DrivingCost{
         }
         
     }//end of main
+    
+    //Functions
+    
+    //Verify input
+    public static boolean verifyInput(Scanner scanInput, double input){
+        
+        if (scanInput.hasNextDouble()){
+            
+            input = scanInput.nextDouble();
+            
+                if (input < 0){
+                    double badInputDoub = input;
+                    System.out.print("\nRe-enter the distance: ");
+                    return true;
+                        
+                }
+                else
+                    return false;
+        }
+        else{
+            String badInputStr = scanInput.next();
+            System.out.print("\nRe-enter the distance: ");
+            return true;
+        }
+        
+    }
+    
+    //Calculation
     
 }//end of class
