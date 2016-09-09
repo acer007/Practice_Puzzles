529d3dba64577df7599f5ccbdee3b93f391cec2d
Nehla <nnamin1446@gmail.com>
2016-09-09 01:13:18 +0000

    No change

--- Hello_World/HelloWorld.java
+++ Hello_World/HelloWorld.java
@@ -3,6 +3,11 @@ public class HelloWorld{
     public static void main(String args[]){
         
         System.out.println("Hello There!");
-        
+        int x = 5;
+        changeVar(x);
+        System.out.println("x = " + x);
+    }
+    public static void changeVar(int a){
+        a++;
     }
 }
