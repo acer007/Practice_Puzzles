997ac0eb13486fd8e688d56729bb843095529aaf
Nehla <nnamin1446@gmail.com>
2016-09-09 16:59:46 +0000

    Initial Commit

---
+++ Chapter 3/Q3.9/CheckISBN10.java
@@ -0,0 +1,47 @@
+// Checksum
+
+import java.util.Scanner;
+
+public class CheckISBN10{
+    
+    public static void main (String args[]){
+        
+        try{
+            
+            //Declare Scanner and Variables
+            Scanner scanInput = new Scanner(System.in);
+            
+            
+            //Prompt input
+            
+            
+            
+            //Verify input
+            
+            
+            
+            //Find Checksum
+            
+            
+            
+        }
+        
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        
+    }//end of main
+    
+    //Method
+    
+    /* findCheckSum
+     * Parameters: String
+     * return: void
+     */
+    public static void findCheckSum(String isbn){
+        
+        
+    }
+    
+    
+}//end of class
