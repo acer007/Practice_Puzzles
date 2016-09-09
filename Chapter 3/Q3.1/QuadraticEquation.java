8a47bc46fefc4c348819ce136eadfb08c1f7e02e
Nehla <nnamin1446@gmail.com>
2016-09-09 00:59:15 +0000

    Inital Commit

---
+++ Chapter 3/Q3.1/QuadraticEquation.java
@@ -0,0 +1,41 @@
+// Allow the user to enter coefficients for a quadratic equation
+// Thr program will find the solutions
+
+import java.util.Scanner;
+
+public class QuadraticEquation{
+    
+    public static void main (String args[]){
+        
+        try{
+            
+            //Declare Scanner and Variables
+            Scanner scanInput = new Scanner(System.in);
+            double a = 0, b = 0, c = 0;
+            double sol1, sol2;
+            
+            //Prompt input
+            
+            
+            //Verify all inputs
+            
+            
+            //Calculate Solution
+            
+            
+            //Display
+            
+        }
+        
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        
+    }//end of main
+    
+    //Methods
+    
+    //Verify input
+    
+    
+}//end of class
