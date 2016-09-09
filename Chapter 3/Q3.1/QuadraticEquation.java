8e41eeb20911617f3c67bb0c053b17e0236c60d0
Nehla <nnamin1446@gmail.com>
2016-09-09 01:49:52 +0000

    Complete Program

--- Chapter 3/Q3.1/QuadraticEquation.java
+++ Chapter 3/Q3.1/QuadraticEquation.java
@@ -13,64 +13,76 @@ public class QuadraticEquation{
             Scanner scanInput = new Scanner(System.in);
             double a = 0, b = 0, c = 0;
             double sol1 = 0, sol2 = 0;
-            boolean sentinel1 = true, sentinel2 = true, sentinel3 = true;
+            boolean cont = true, sentinel1 = true, sentinel2 = true, sentinel3 = true;
             double discriminant = 0;
             
-            //Prompt input
-            System.out.print("For ax^2 + bx + c, enter a, b, and c: ");
-            
-            //Verify all inputs
-            while (sentinel1){
-                a = verifyInput(scanInput);
+            /*while (cont){*/
                 
-                if (a != -1){
-                    sentinel1 = false;
+                //Prompt input
+                System.out.print("For ax^2 + bx + c, enter a, b, and c \nEnter 'q' to quit: ");
+                
+                /*if (scanInput.next() != "q"){*/
+                    
+                    //Verify all inputs
+                    while (sentinel1){
+                        a = verifyInput(scanInput);
+                        
+                        if (a != -1){
+                            sentinel1 = false;
+                        }
+                        
+                    }
+                    
+                    while (sentinel2){
+                        b = verifyInput(scanInput);
+                        
+                        if (b != -1){
+                            sentinel2 = false;
+                        }
+                    }
+                    
+                    while (sentinel3){
+                        c = verifyInput(scanInput);
+                        
+                        if (c != -1){
+                            sentinel3 = false;
+                        }
+                        
+                    }
+                    
+                    //Find Discriminant
+                    discriminant = findDiscriminant(a, b, c);
+                    
+                    //Calculate Solution
+                    if (discriminant > 0){
+                        
+                        sol1 = findPos(a, b, c);
+                        sol2 = findNeg(a, b ,c);
+                        System.out.println("The solutions are " + sol1 + " and " +
+                                            sol2);
+                        
+                    }
+                    
+                    else if (discriminant == 0){
+                        
+                       sol1 = findPos(a, b, c);
+                       System.out.println("The solution is " + sol1);
+                        
+                    }
+                    
+                    else{
+                        System.out.println("The equation has no real roots");
+                    }
+                
+                /*}//end of if statement
+                
+                else{
+                    cont = false;
                 }
-                
-            }
             
-            while (sentinel2){
-                b = verifyInput(scanInput);
-                
-                if (b != -1){
-                    sentinel2 = false;
-                }
-            }
+            }//end of while loop*/
             
-            while (sentinel3){
-                c = verifyInput(scanInput);
-                
-                if (c != -1){
-                    sentinel3 = false;
-                }
-                
-            }
-            
-            //Find Discriminant
-            discriminant = findDiscriminant(a, b, c);
-            
-            //Calculate Solution
-            if (discriminant > 0){
-                
-                sol1 = findPos(a, b, c);
-                sol2 = findNeg(a, b ,c);
-                System.out.println("The solutions are " + sol1 + " and " +
-                                    sol2);
-                
-            }
-            
-            else if (discriminant == 0){
-                
-               sol1 = findPos(a, b, c);
-               System.out.println("The solution is " + sol1);
-                
-            }
-            
-            else{
-                System.out.println("The equation has no real roots");
-            }
-            
-        }
+        }//end of try block
         
         catch (Exception e){
             System.out.println(e.getMessage());
