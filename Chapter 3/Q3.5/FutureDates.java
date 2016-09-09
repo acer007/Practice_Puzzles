e83d4c28483e89b22dc613e336f8a145580df96c
Nehla <nnamin1446@gmail.com>
2016-09-09 15:41:20 +0000

    Initial Commit

---
+++ Chapter 3/Q3.5/FutureDates.java
@@ -0,0 +1,31 @@
+// Enter a number corresponding to today's weekday and the number of days 
+// elapsed in order to display the future weekday given the number of days 
+// elapsed : daysElapsed % 7
+
+import java.util.Scanner;
+
+public class FutureDates{
+    
+    public static void main (String args[]){
+        
+        try{
+            
+            //Declare Scanner and variables
+            Scanner scanInput = new Scanner(System.in);
+            String weekDayStart = "";
+            String weekdayEnd = "";
+            
+            //Prompt input
+            
+            //Verify input
+            
+            //Display
+        }
+        
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        
+    }//end of class
+    
+}// end of class
