3b194a69d61134a297d9d1da0917d545d1aa0ac7
Nehla <nnamin1446@gmail.com>
2016-09-11 15:28:01 +0000

    Verified input

--- Chapter 3/Q3.9/CheckISBN10.java
+++ Chapter 3/Q3.9/CheckISBN10.java
@@ -13,15 +13,40 @@ public class CheckISBN10{
             
             
             //Prompt input
-            
-            
+            System.out.println("Enter the 9-digit ISBN number: ");
+            int isbn = 0;
             
             //Verify input
-            
+            while (scanInput.hasNext()){
+                
+                if (scanInput.hasNextInt()){
+                    isbn = scanInput.nextInt();
+                    
+                    if (isbn < 100000000){
+                        int badInputInt = isbn;
+                        System.out.println("Enter the 9-digit ISBN number: ");
+                    }
+                    else
+                        break;
+                }
+                
+                else{
+                    String badInputStr = scanInput.next();
+                    System.out.println("Enter the 9-digit ISBN number: ");
+                }
+            }
             
             
             //Find Checksum
-            
+            String isbnStr = Integer.toString(isbn);
+            int checkSum = 0;
+            for (int k = 0; k < 9; k++){
+                
+                //must copy the checkSum int into a String variable in order to dereference it
+                System.out.println("Number: " + (int)(isbnStr.charAt(k)));
+                checkSum += (int)(isbnStr.charAt(k)) * (k+1);
+                System.out.println("Checksum: " + checkSum);
+            }
             
             
         }
@@ -39,6 +64,7 @@ public class CheckISBN10{
      * return: void
      */
     public static void findCheckSum(String isbn){
+    
         
         
     }
