4307edb9264205ecb451621821f5359df2d46ca5
Nehla <nnamin1446@gmail.com>
2016-09-13 14:19:35 +0000

    Initial Commit

---
+++ Chapter 3/Q3.15/LotteryDigits.java
@@ -0,0 +1,41 @@
+// Win an award (or not) depending on whether the user input matches the 
+// winning lottery numbers
+
+import java.util.Scanner;
+
+public class LotteryDigits{
+    
+    public static void main (String args[]){
+        
+        try{
+            
+            //Declare Scanner
+            Scanner scanInput = new Scanner(System.in);
+            
+            //Generate lottery numbers
+            
+            
+            //Prompt User Input
+            
+            
+            //Verify inputs
+            
+            
+            //Check the guess
+            
+            
+        }
+        
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        
+    }
+    
+    //Method
+    /* Verify input
+     * Parameters: Scanner
+     * return: integer
+     */
+    
+}
