32b04041b4037886bfc1ee7f56adb40b8d6c61a6
Nehla <nnamin1446@gmail.com>
2016-09-16 00:21:40 +0000

    Converted Hexadecimal Digit to Binary Number

--- Chapter 4/Q4.12/HexToBinary.java
+++ Chapter 4/Q4.12/HexToBinary.java
@@ -19,32 +19,96 @@ public class HexToBinary{
             //System.out.println("Hex String: " + hexString);
             //System.out.println("Hex Char: " + hexDigit);
             if (Character.isDigit(hexDigit)){
-                int hexInt = hexString.toInteger();
+                
+                int hex = (int)hexDigit - 48;
+                String binary1 = "";
+                
+                while (hex != 0){
+                    
+                    int rem = hex % 2;
+                    binary1 += Integer.toString(rem);
+                    hex = hex / 2;
+                    
+                }
+                if (binary1.length() < 4){
+                    
+                    while (binary1.length() != 4){
+                        binary1 += "0";
+                    }
+                    
+                }
+                binary1 = palindrome(binary1);
+                System.out.println("Binary Number: " + binary1);
+                
             }
             
-            if (Character.isUpperCase(hexDigit)){
+            else if (Character.isUpperCase(hexDigit)){
                 
                 //check if capital letter ascii code is between 65 - 70
+                int hex = (int)hexDigit - 65;
+                String binary1 = "";
                 
-                
-                //invalid
-                
+                if (hex <= 5){
+                    
+                    hex += 10;
+                    
+                    while (hex != 0){
+                    
+                        int rem = hex % 2;
+                        binary1 += Integer.toString(rem);
+                        hex = hex / 2;
+                    
+                    }
+                    
+                    if (binary1.length() < 4){
+                        
+                        while (binary1.length() != 4){
+                            binary1 += "0";
+                        }
+                        
+                    }
+                    binary1 = palindrome(binary1);
+                    System.out.println("Binary Number: " + binary1);
+                    
+                }
                 
             }
             
-            if (Character.isLowerCase()){
+            else if (Character.isLowerCase(hexDigit)){
                 
                 //check if capital letter ascii code is between 97 - 102
+                int hex = (int)hexDigit - 97;
+                String binary1 = "";
                 
-                
-                //invalid
-                
+                if (hex <= 5){
+                    
+                    hex += 10;
+                    
+                    while (hex != 0){
+                    
+                        int rem = hex % 2;
+                        binary1 += Integer.toString(rem);
+                        hex = hex / 2;
+                    
+                    }
+                    
+                    if (binary1.length() < 4){
+                        
+                        while (binary1.length() != 4){
+                            binary1 += "0";
+                        }
+                        
+                    }
+                    binary1 = palindrome(binary1);
+                    System.out.println("Binary Number: " + binary1);
+                    
+                }
                 
             }
             
             //invalid
             else{
-                System.out.println(hexDigit + "is an invalid input");
+                System.out.println(hexDigit + " is an invalid input");
             }
             
             
@@ -54,6 +118,21 @@ public class HexToBinary{
             System.out.println(e.getMessage());
         }
         
+    }
+    
+    /* palindrome
+     * Parameters: String
+     * return: String
+     */
+     
+    public static String palindrome (String sequence){
+        String copy = "";
+        
+        for (int k = sequence.length(); k >= 1; k--){
+            copy += sequence.charAt(k-1);
+        }
+        
+        return copy;
     }
     
 }
