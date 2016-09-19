03372f0fe4ce7219a92f17e412285dfdaaba7594
Nehla <nnamin1446@gmail.com>
2016-09-19 12:39:37 +0000

    Modified for loops

--- Chapter 5/Q5.18/FourStairs.java
+++ Chapter 5/Q5.18/FourStairs.java
@@ -58,37 +58,24 @@ public class FourStairs{
             
             for (int  i = lines; i > 0; i--){
                 
-                for (int k = 1; k < i; k++){
+                for (int k = 1; k <= i; k++){
                     keyB += k + " ";
                 }
                 System.out.println(keyB);
                 keyB = "";
             }
             
-            /*for (int j = lines; j > 0; j--){
-                
-                for (int k = (lines - j); k > 0; k--){
-                    
-                    keyB = (keyB) + " " + k;
-                    
-                }
-                
-                System.out.println(keyB);
-                keyB = "";
-                
-            }*/
-
-            
+ 
             
             //Pattern C
             System.out.println("Pattern C");
             
-            for (int i = 0; i < lines; i++){
+            for (int i = 1; i <= lines; i++){
                 
-                for (int k = 1; k < (lines - (i)); k++){
+                for (int k = 0; k < (lines - (i)); k++){
                     System.out.printf("  ");    
                 }
-                keyC = (i+1) + " " + keyC;
+                keyC = (i) + " " + keyC;
                 System.out.println(keyC);
                 
             }
@@ -97,8 +84,21 @@ public class FourStairs{
             //Pattern D
             System.out.println("Pattern D");
             
-            
-            
+            for (int j = 0; j < lines; j++){
+                
+                for (int k = lines; k > (lines - j); k--){
+                    System.out.printf("  ");
+                }
+                
+                for (int l = 1; l < (lines - j); l++){
+                    
+                    keyD += l + " ";
+                }
+                
+                System.out.println(keyD);
+                keyD = "";
+                
+            }
             
         }
         
